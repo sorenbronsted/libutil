2acfdc712724bce695ddbc50f28ed8b8d90d42a1
Søren Brønsted <sbr@ufds.dk>
2016-03-23 16:07:57 +0100

    Ported to new build system

--- test/settings.php
+++ test/settings.php
@@ -1,21 +1,7 @@
 <?php
 
-spl_autoload_register(function($class) {
-	$paths = array(
-		"di",
-		"config",
-		"log",
-		"test/log",
-	);
+$loader = require 'vendor/autoload.php';
+$loader->addClassMap(array(
+	'TestWriter' => 'test/log/TestWriter.php',
+));
 
-	foreach($paths as $path) {
-		$fullname = $path.'/'.$class.'.php';
-		if (is_file($fullname)) {
-			include($fullname);
-			return true;
-		}
-	}
-	return false;
-});
-
-?>
